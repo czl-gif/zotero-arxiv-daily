530bd9233cfe6f31dc8cca71dfbc26fe62c7bda1
Chen Zilong <75004282+czl-gif@users.noreply.github.com>
2025-11-13 17:12:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def get_arxiv_paper(query:str, debug:bool=False) -> list[ArxivPaper]:
         papers = []
         all_paper_ids = [i.id.removeprefix("oai:arXiv.org:") for i in feed.entries if i.arxiv_announce_type == 'new']
         bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
-        for i in range(0,len(all_paper_ids),20):
-            search = arxiv.Search(id_list=all_paper_ids[i:i+20])
+        for i in range(0,len(all_paper_ids),10):
+            search = arxiv.Search(id_list=all_paper_ids[i:i+10])
             batch = [ArxivPaper(p) for p in client.results(search)]
             bar.update(len(batch))
             papers.extend(batch)
